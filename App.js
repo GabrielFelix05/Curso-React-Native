f9bd93f2dc1ea6dffec16874d6a4c4e6dba38e6c
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-07-22 21:06:15 -0300

    Slider

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
 import React, { useState } from 'react'
 import {View, Text, StyleSheet} from 'react-native'
-import { Picker } from '@react-native-picker/picker'
+import Slider from '@react-native-community/slider'
 
 function App(){
-  const [carroSelecionado, setCarroSelecionado] = useState(0)
-  const [carros, setCarros] = useState([
-    {key: 1, nome: "Golf 1.6", valor: 62.000},
-    {key: 2, nome: "Saveiro", valor: 29.300},
-    {key: 3, nome: "Gol 1.0", valor: 25.200},
-    {key: 4, nome: "bmw", valor: 109.000}
-  ])
-
-  let carrosItens = carros.map((v, k) => {
-    return <Picker.Item key={k} value={k} label={v.nome}/>
-  })
+  const [valor, setValue] = useState(50)
   return(
     <View style={styles.container}>
-     <Picker
-      selectedValue={carroSelecionado}
-      onValueChange={(itemValue, itemIndex) => setCarroSelecionado(itemValue)}
-     >
-       {carrosItens}
-     </Picker>
-      <Text>
-        R$: {carros[carroSelecionado].valor.toFixed(3)}
-      </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        value={valor}
+        onValueChange={(valorSelecionado) => setValue(valorSelecionado)}
+        minimumTrackTintColor="#000fff"
+        maximumTrackTintColor="#ff0000"
+        thumbTintColor="#ff0000"
+      />
+
+      <Text style={{textAlign: 'center', fontSize: 30, marginTop: 20}}> Valor: {valor.toFixed(0)}</Text>
     </View>
   )
 }
