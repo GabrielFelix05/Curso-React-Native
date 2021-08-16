21b6626220d9a1b8ffe99a9f7758760e59b29f1f
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-08-16 15:35:21 -0300

    useState  useEffect

--- App.js
+++ App.js
@@ -1,56 +1,29 @@
-import React, {useState} from 'react'
-import {View, Text, StyleSheet, FlatList} from 'react-native'
-import Header from './src/header'
-import List from './src/list'
+import React, {useState, useEffect} from 'react'
+import {View, Text, StyleSheet, Button} from 'react-native'
 
 function App(){
-  const [feed, setFeed] = useState([
-    {
-      id: 1, 
-      nome: "Gabriel Felix", 
-      descricao: "Mais um dia de muitos bugs", 
-      imgperfil: "https://sujeitoprogramador.com/instareact/fotoPerfil1.png", 
-      publicacao: "https://sujeitoprogramador.com/instareact/foto1.png", 
-      likeada: true, 
-      likers: 0
-    },
-    {
-      id: 2, 
-      nome: "Felix", 
-      descricao: "Mais um dia de muitos bugs", 
-      imgperfil: "https://sujeitoprogramador.com/instareact/fotoPerfil2.png", 
-      publicacao: "https://sujeitoprogramador.com/instareact/foto2.png", 
-      likeada: false, 
-      likers: 1
-    },
-    {
-      id: 2, 
-      nome: "Silva", 
-      descricao: "Mais um dia de muitos bugs", 
-      imgperfil: "https://sujeitoprogramador.com/instareact/fotoPerfil3.png", 
-      publicacao: "https://sujeitoprogramador.com/instareact/foto3.png", 
-      likeada: false, 
-      likers: 30
-    }
-  ]) 
+  const [contador, setContador] = useState(0)
+
+  useEffect(() => {
+    console.log('MONTOU')
+  }, [contador])
 
   return(
     <View style={styles.container}>
-      <Header/>
-        
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
-        data={feed}
-        renderItem={({item}) => <List data={item}/>}
-      />
+      <Button title={'Aumentar'} onPress={() => setContador(contador+1)}/>
+      <Text style={{fontSize: 30}}>
+        {contador}
+      </Text>
+      <Button title={'Diminuir'} onPress={() => setContador(contador-1)}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
