6d0025aa07518d6ce927fc7cd95b5bb44107f9bb
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2022-02-21 22:17:24 -0300

    Primeiros passos com React Native

--- App.js
+++ App.js
@@ -1,77 +1,55 @@
-import React, {useState, useEffect, useMemo} from 'react'
-import {View, Text, StyleSheet, Button, TextInput, TouchableOpacity} from 'react-native'
-import AsyncStorange from '@react-native-async-storage/async-storage'
+import React from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
 function App(){
-  const [input, setInput] = useState('')
-  const [nome, setNome] = useState('')
-
-  useEffect(() => {
-    async function LoadData(){
-      await AsyncStorange.getItem('@nome').then((value) => {
-        setNome(value)
-      })
-    }
-
-    LoadData()
-  },[])
-
-  async function gravaNome(){
-    await AsyncStorange.setItem('@nome', input)
-    setNome(input)
-    setInput('')
-  }
-
-  const letrasNome = useMemo(() => {
-    console.log('Pegando quantidade de letras ')
-    return nome.length
-  }, [nome])
+  
+  let data = new Date().toLocaleString()
 
   return(
     <View style={styles.container}>
-      <View style={styles.viewInput}>
-        <TextInput
-          style={styles.input}
-          value={input}
-          onChangeText={(texto) => setInput(texto)}
-        />  
-        <TouchableOpacity onPress={gravaNome}>
-          <Text style={styles.botao}>+</Text>
-        </TouchableOpacity>
+      <View style={styles.button}>
+        <Text style={styles.textButton}>React Native</Text>
       </View>
-
-      <Text style={styles.nome}>{nome}</Text>
-      <Text style={styles.nome}>Possui: {letrasNome > 1 ? letrasNome + ' letras' : letrasNome + ' letra'}</Text>
+      <Image
+        source={{uri: "https://sujeitoprogramador.com/reactlogo.png"}}
+        style={styles.logo}
+      />
+      <Text style={styles.data}>{data}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     alignItems: 'center',
-    marginTop: 35
+    justifyContent: 'center',
+    backgroundColor: "#6E6E6E",
   },
-  viewInput:{
-    flexDirection: "row",
-    alignItems: 'center'
-  },
-  input:{
-    width: 350,
-    height: 40,
-    borderColor: '#000',
-    borderWidth: 1,
+  button: {
+    alignItems: 'center',
+    borderWidth: 3,
+    borderColor: "#fff",
     padding: 10,
+    borderRadius: 5,
+    width: 200,
   },
-  botao:{
-    backgroundColor: '#222',
-    color: '#fff',
-    height: 40,
-    padding: 10,
-    marginLeft: 4
+  textButton: {
+    fontSize: 20,
+    fontWeight: "bold",
+    letterSpacing: 2,
+    color: "#fff",
   },
-  nome:{
-    fontSize: 30
+  logo: {
+    width: 320,
+    height: 300,
+  },
+  data: {
+    color: "#fff",
+    fontSize: 23,
+    paddingTop: 30,
+    fontWeight: "bold",
+    letterSpacing: 1,
   }
 })
 
